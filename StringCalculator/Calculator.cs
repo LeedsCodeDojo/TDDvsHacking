6099fa5d46d39bb82b4358a1db4b2b408b9ffb5d
Adam Pridmore <adam_pridmore@hotmail.com>
2017-04-05 20:01:42 +0100

    - Initial commit. May work (or not).

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+            {
+                return 0;
+            }
+
+            if (numbers.StartsWith("//"))
+            {
+                return CustomDelimiters(numbers);
+            }
+            else
+            {
+                return SumNumbersText(numbers, new[] {",", "\n"});
+            }
+        }
+
+        private static int CustomDelimiters(string numbers)
+        {
+            string firstLineText = numbers.Split('\n').First();
+            string delimiter = firstLineText.Substring(2, firstLineText.Length - 3);
+            if (delimiter == string.Empty) // Delimiter is a \n
+            {
+                string restOfText = string.Join("\n", numbers.Split('\n').Skip(1));
+                return SumNumbersText(restOfText, new[] {"\n"});
+            }
+            else
+            {
+                string restOfText = numbers.Split('\n').Skip(1).Single();
+
+                if (firstLineText.Contains("["))
+                {
+                    string[] delimiters = firstLineText
+                        .Split(']')
+                        .Select(txt => txt.Substring(1, txt.Length - 2))
+                        .ToArray()
+                        ;
+
+                    return SumNumbersText(restOfText, delimiters);
+                }
+                else
+                {
+                    return SumNumbersText(restOfText, new[] {delimiter});
+                }
+            }
+        }
+
+        private static int SumNumbersText(string numbers, string[] delimiters)
+        {
+            AssertValidNumbersText(numbers);
+
+            var intList = numbers
+                .Split(delimiters, StringSplitOptions.None)
+                .Select(int.Parse)
+                .Where(i => i <= 1000);
+
+            if (intList.Any(i => i < 0))
+            {
+                throw new ArgumentException("Negatives not allowed");
+            }
+
+            return intList.Sum();
+        }
+
+        private static void AssertValidNumbersText(string numbers)
+        {
+            if (numbers.Contains(",\n"))
+            {
+                throw new ArgumentException("Invalud numbers text. Contains ',\\n'");
+            }
+
+            if (numbers.Contains("\n,"))
+            {
+                throw new ArgumentException("Invalud numbers text. Contains '\\n,'");
+            }
+        }
+    }
+}
--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StringCalculator
+﻿namespace StringCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
